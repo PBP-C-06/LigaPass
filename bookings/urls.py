1071e485a061bb46ea0c6fe10c794b82b8e7fdb7
Jaysen Lestari <lestarijaysen@gmail.com>
2025-10-22 20:48:39 +0700

    Refactor payment logic

--- bookings/urls.py
+++ bookings/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("payment/<uuid:booking_id>/", payment, name="payment"),
     path("cancel/<uuid:booking_id>/", cancel_booking, name="cancel_booking"),
     path('notification/', midtrans_notification, name='midtrans_notification'),
+    path("check_status/<uuid:booking_id>/", check_booking_status, name="check_booking_status"),
 ]
