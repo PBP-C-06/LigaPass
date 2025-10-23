b647f3faa485bd10acef03c0a315cb4c8539fa81
RefkiSeptian <refkiseptianrs@gmail.com>
2025-10-23 19:29:11 +0700

    feat/analytics-review perbaikan di view

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,18 +4,16 @@ from . import views, views_analytics
 app_name = "reviews"
 
 urlpatterns = [
-    # Entry tanpa match_id → akan redirect ke match terakhir / match terakhir dari tim terpilih
     path("user/", views.user_review_entry, name="user_review_entry"),
-    # Halaman user untuk 1 match tertentu
     path("user/<uuid:match_id>/", views.user_review_page, name="user_review_page"),
 
-    # API AJAX
+
     path("api/<uuid:match_id>/create/", views.api_create_review, name="api_create_review"),
     path("api/<uuid:match_id>/update/", views.api_update_review, name="api_update_review"),
     path('admin/', views.admin_review_page, name='admin_review_page'),
     path('api/reply/<uuid:review_id>/', views.api_add_reply, name='api_add_reply'),
 
-    # Analytics URLs
+    
     path("analytics/admin/", views_analytics.admin_analytics_page, name="admin_analytics_page"),
     path("analytics/admin/data/", views_analytics.api_admin_analytics_data, name="api_admin_analytics_data"),
     path("analytics/user/", views_analytics.user_analytics_page, name="user_analytics_page"),
