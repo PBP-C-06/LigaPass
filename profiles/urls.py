1e03812aac3f7c155c6f96f4a1373e76277974e2
applepiesss <nadiaaiiisyahf@gmail.com>
2025-10-23 17:18:36 +0700

    Added AJAX to navbar, Changed urls for readability, and Updated UI for journalist profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,14 +4,21 @@ from profiles.views import admin_change_status, admin_view, create_profile, edit
 app_name = 'profiles'
 
 urlpatterns = [
+    # Untuk create profile
     path('user/create-profile/', create_profile, name='create_profile'),
-    path('json/', show_json, name='show_json'),
+
+    # Untuk endpoints
+    path('json/adminjournalist/', show_json_admin_journalist, name='show_json_admin_journalist'),
+    path('json/adminview/', admin_view_json, name='admin_view_json'),
     path('json/<uuid:id>/', show_json_by_id, name='show_json_by_id'),
-    path('adminJournalistJson/', show_json_admin_journalist, name='show_json_admin_journalist'),
-    path('adminViewJson/', admin_view_json, name='admin_view_json'),
+    path('json/', show_json, name='show_json'),
+
+    # Untuk view pages
     path("user/<uuid:id>/", user_view, name="user_view"),
     path("admin/", admin_view, name="admin_view"),
     path("journalist/", journalist_view, name="journalist_view"),
     path('user/<uuid:id>/edit/', edit_profile_for_user, name='edit_profile_for_user'), 
+
+    # Untuk admin edit status user
     path("admin/edit/<uuid:id>/", admin_change_status, name="admin_change_status"),
 ]
